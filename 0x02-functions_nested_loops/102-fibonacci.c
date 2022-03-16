c8c96c9e2554b390463105853db66f40ed93b1a1
Adewale IyanuOluwa Isaac <60206492+Ultra-Tech-code@users.noreply.github.com>
2022-03-16 21:50:37 +0100

    added 102-fibonacci.c file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,14 +3,13 @@
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  * followed by a new line
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
 	long int i, j, k, next;
 
 	j = 1;
-
 	k = 2;
 
 	for (i = 1; i <= 50; ++i)
