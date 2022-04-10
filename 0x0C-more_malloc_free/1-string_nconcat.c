220593bb99706c2539a5ab4be7b11631dabd65b4
Adewale IyanuOluwa Isaac <60206492+Ultra-Tech-code@users.noreply.github.com>
2022-04-10 18:59:49 +0100

    edited the files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,36 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
- * string_nconcat - concatenates two stringa.
- * @s1: string to be concatenated.
- * @s2: string to be concatenated.
- * @n: first bytes of s2.
- * Return: String concatenated.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *array = NULL;
-unsigned int i = 0, j = 0, k;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-j++;
-if (n < j)
-j = n;
-array = malloc(((i + j + n) * sizeof(char) + 1));
-if (array == NULL)
-return (NULL);
-for (k = 0; k < i; k++)
-array[k] = s1[k];
-i = k;
-for (k = 0; k < j; k++, i++)
-array[i] = s2[k];
-array[i] = '\0';
-return (array);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
